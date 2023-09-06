065e12058b03241e781d8e420d78ccd2a06e7643
gabriel1032 <santi1320@gmail.com>
2023-09-06 18:01:50 -0500

    se ñadio una carpeta

---
+++ M08_clasesyOOP/miModulo.py
@@ -0,0 +1,14 @@
+# miModulopy
+
+def suma(a , b):
+    return a + b
+    
+def resta(a , b):
+    return a - b
+
+def multipli(a , b):
+    return a * b 
+
+def divi(a , b):
+    return a / b  
+    
